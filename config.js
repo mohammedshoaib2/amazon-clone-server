cdac9ed8deb09d2ab093160b0f8950bf9fee5c5e
mohammedshoaib2 <133799128+mohammedshoaib2@users.noreply.github.com>
2024-05-09 18:50:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
-const url = "mongodb+srv://UsernameOfMongo:PassowordOfMongo@cluster0.escm503.mongodb.net/Amazon-Clone?retryWrites=true&w=majority"
+const url = "mongodb+srv://mohammedshoaib733093:Shoaib5400@cluster0.escm503.mongodb.net/Amazon-Clone?retryWrites=true&w=majority";
 mongoose.connect(url);
